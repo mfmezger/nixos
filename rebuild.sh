d4085f75a4c4e0c352563dbb5a4656d7aa0783dd
Marc Fabian Mezger <57255687+mfmezger@users.noreply.github.com>
2024-06-23 19:05:11 +0000

    adding rebuild script inspired by no boilerplate code.

---
+++ rebuild.sh
@@ -0,0 +1,15 @@
+alejandra .
+echo "Formatting done."
+
+# Rebuild, output simplified errors, log trackebacks
+sudo nixos-rebuild switch &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+
+# Get current generation metadata
+current=$(nixos-rebuild list-generations | grep current)
+
+# Commit all changes witih the generation metadata
+git commit -am "$current"
+git push
+
+# Notify all OK!
+notify-send -e "NixOS Rebuilt OK!" --icon=software-update-available
