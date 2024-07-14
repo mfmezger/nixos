eded6f99b35a0a298bbc409ad9f05fd1978b2c27
Marc Fabian Mezger <marc.mezger@gmail.com>
2024-07-14 22:58:33 +0200

    49 current  2024-07-14 22:37:04  24.11.20240622.a71e967   6.6.35                          *

--- flake.nix
+++ flake.nix
@@ -36,6 +36,7 @@
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+        extraSpecialArgs = {inherit inputs;};
         modules = [
           ./configuration.nix
 
