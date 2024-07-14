7623a6e5bc591337480a9cf20a8add5ab515f650
Marc Fabian Mezger <marc.mezger@gmail.com>
2024-07-14 21:37:47 +0200

    41 current  2024-07-14 21:36:43  24.11.20240622.a71e967   6.6.35                          *

--- home.nix
+++ home.nix
@@ -48,22 +48,23 @@
       ll = "ls -l";
 
       # GIT
-      ghcs="gh copilot suggest";
-      gs="git status";
-      gg="git add . && git commit -m";
-      gp="git push";
+      ghcs = "gh copilot suggest";
+      gs = "git status";
+      gg = "git add . && git commit -m";
+      gp = "git push";
 
       # NIX
       update = "sudo nixos-rebuild switch";
-      
+
       # EZA is a custom alias for ls -lah
-      alias ls="eza -lah";
-      l="eza -lah";
-      cat="bat";
-      cd="z";
-  };
+      ls = "eza -lah";
+      l = "eza -lah";
+      cat = "bat";
+      cd = "z";
+    };
     history = {
-    size = 30000;};
+      size = 30000;
+    };
   };
   programs.atuin = {
     enable = true;
